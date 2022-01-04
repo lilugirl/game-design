129cf7dad5156e608c97f83e33b2abbdc77afd81
liu yi <lilugirl2005@126.com>
2022-01-04 19:17:07 +0800

    mapping

--- chapter01/index.js
+++ chapter01/index.js
@@ -33,3 +33,26 @@ if(found2) console.log('tabla found!');
 let numbers=[11,43,9,112,64,15];
 let tooBig=numbers.some(number=>number>100);
 console.log(tooBig);
+
+
+// Finding Array Elements
+let found3=instruments.find(x=>x==='tabla');
+console.log(found3); // display tabla
+
+let index=instruments.findIndex(x=>x==='tabla');
+console.log(index); // display 2
+
+
+// Mapping an Old Array to a New array
+let words=["fun","boring","exciting"];
+function improveGrammar(word){
+    return word+'ish';
+}
+let betterWords=words.map(improveGrammar);
+
+console.log(betterWords);
+
+// ultra-compact way
+let betterWords2=words.map(x=>x+'ish');
+console.log(betterWords2);
+
